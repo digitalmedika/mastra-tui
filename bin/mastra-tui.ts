ac70031d699eaf291070c94c940985b239980e23
billymontolalu <billy.montolalu@gmail.com>
2026-06-17 16:05:11 +0700

    feat: add version and help options to mastra-tui command

--- bin/mastra-tui.ts
+++ bin/mastra-tui.ts
@@ -1,3 +1,21 @@
 #!/usr/bin/env bun
 
+const [, , ...args] = process.argv;
+
+if (args.includes('-v') || args.includes('--version')) {
+  const pkg = await import('../package.json', { with: { type: 'json' } });
+  console.log(pkg.default.version);
+  process.exit(0);
+}
+
+if (args.includes('-h') || args.includes('--help')) {
+  console.log(`Usage: loccle [options]
+
+Options:
+  -v, --version  Show version number
+  -h, --help     Show this help message
+`);
+  process.exit(0);
+}
+
 await import('../src/tui/index');
